57a4311b36ac4f9d2cf5b542bcf75f03ab3712e6
Krish Joshi <krish@xoxoday.com>
2023-12-21 14:19:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './Login';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Signup from './Signup';
+import Dash from './Dash';
+import Plans from './Plans';
+import Users from './Users';
+import Settings from './Settings';
+import Userprofile from './userprofile';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element= {<Login />} ></Route>
+            <Route path='/signup' element={<Signup />}></Route>
+            <Route path='/Dash' element={<Dash />}></Route>
+            <Route path='/plans' element={<Plans />}></Route>
+            <Route path='/Users' element={<Users />}></Route>
+            <Route path='/userprofile' element={<Userprofile />}></Route>
+            <Route path='/Settings' element={<Settings />}></Route>
+        </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
+export default App
